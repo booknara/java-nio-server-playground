c457e51134c41dd3b2c6b39a95789359607fb0d7
Daehee Han <bookdori81@gmail.com>
2016-03-01 18:39:44 -0800

    Optimize imports

--- src/com/github/booknara/server/classic/client/CapitalizeClient.java
+++ src/com/github/booknara/server/classic/client/CapitalizeClient.java
@@ -1,6 +1,8 @@
 package com.github.booknara.server.classic.client;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 
 /**
